7aafa69f7eea1a8c9f1deb4a457c2bd7379d42ca
Damian <damczys@gmail.com>
2023-09-06 20:23:26 +0200

    adding first feature files with steps

---
+++ Feature/Steps/LoginScenarios.py
@@ -0,0 +1,44 @@
+from  behave import *
+
+@given("User is on Swag Labs")
+def user_is_on_swag_labs(context):
+    print("Page is opened")
+
+@given("User enters accepted username")
+def user_enters_accepted_username(context):
+    print ("geting and puting accepted username")
+
+@given("User enters locked username")
+def user_enters_locked_username(context):
+    print ("geting and puting locked username")
+
+@given("User enters problem username")
+def user_enters_problem_username(context):
+    print ("geting and puting problem username")
+
+@given("User enters accepted password")
+def user_enters_accepted_password(context):
+    print ("password is inserted")
+
+@when("User clicks Login button")
+def user_clicks_Login_button(context):
+    print ("login button is pressed")
+
+@then("User is on invetory page")
+def user_is_on_invetory_page(context):
+    print ("invertory page is displayed")
+
+@then("'{text}' is displayed")
+def is_displayed(context, text: str):
+    print (f"{text} is displayed")
+
+@then("message error is displayed '{text}'")
+def  message_error_is_displayed(context, text: str):
+    print (f"{text} is displayed ")
+    #"Epic sadface: Sorry, this user has been locked out."
+
+@then("User is on swag labs page")
+def user_is_on_swag_labs_page(context):
+    print ("checks that user is on swah lab page")
+
+
--- Helpers/Common.py
+++ Helpers/Common.py
@@ -2,16 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from typing import Union
 
-def startUp():
-    driver = webdriver.Firefox()
-    driver.get("https://www.netwars.pl")
-    locator = ".topic_list th:nth-child(1)"
-    driver.set_page_load_timeout(30)
-    try:
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, locator)))
-        print (element.text)
-    finally:
-        driver.quit()
+typeWebDriver = Union[webdriver.Firefox,
+                      webdriver.Chrome,
+                      webdriver.Safari
+]
 
-startUp()
+
+# locator = ".topic_list th:nth-child(1)"
+class Common():
+    def __init__(self, driver: typeWebDriver):
+        self.driver: typeWebDriver = driver
+        self.driver.get("https://www.saucedemo.com")
+
+    def presance_of_element(self, by: By, locator: str, timeout: int = 10) -> WebDriverWait:
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((by.CSS_SELECTOR, locator)))
+
+    def element_to_be_clickable(self, by: By, locator: str, timeout: int = 10) -> WebDriverWait:
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((by.CSS_SELECTOR, locator)))
+
+    def element_to_be_selected(self, by: By, locator: str, timeout: int = 10) -> WebDriverWait:
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_selected((by.CSS_SELECTOR, locator)))
+
+    def __del__(self):
+        self.driver.quit()
